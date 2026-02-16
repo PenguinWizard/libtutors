fa3a27ca204b7f517af3e41e9e91b3b9b95cf561
ArmanPefourque <armanpefo@gmail.com>
2026-02-15 18:09:30 -0800

    Feedback form and google sheets attendence system

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-const supabaseClient = supabase.createClient(
-    "null",
-    "null"
-  );
-
 document.addEventListener("DOMContentLoaded", () => {
   const current = localStorage.getItem("attendance");
 
@@ -19,38 +14,54 @@ function markAttendance() {
   if (current === "True") {
     localStorage.setItem("attendance", "False");
     document.getElementById("signup_button").textContent = "Mark as coming";
-    decreaseAttendance(1);
+    decreaseAttendance();
   } else {
     localStorage.setItem("attendance", "True");
     document.getElementById("signup_button").textContent = "Unmark as coming";
-    increaseAttendance(1);
+    increaseAttendance();
   }
 }
 
-async function increaseAttendance(id) {
-  const { data, error } = await supabaseClient
-    .from('attendance')
-    .update({ attendance_num: supabaseClient.increment(1) })
-    .eq('id', id)
-    .select();
+async function increaseAttendance() {
+  try {
+    const response = await fetch("https://script.google.com/macros/s/AKfycbztzl7tuBZDQDlSsfsHbfzwyZoP4Owb1bC-Q7zH-Knu8n8Q6i7xhf-yff8WhfnAOv4MoA/exec", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ change: 1 })
+    });
 
-  if (error) {
-    console.log("Error:", error);
-  } else {
-    console.log("Updated:", data);
+    const result = await response.json();
+
+    if (result.success) {
+      alert("Attendance sucesfully recorded!");
+    } else {
+      alert("Sorry, a problem has occured. Please check back in a few minutes.");
+    }
+
+  } catch (error) {
+    console.error("Increase error:", error);
+    alert("Network or script error");
   }
 }
 
-async function decreaseAttendance(id) {
-  const { data, error } = await supabaseClient
-    .from('attendance')
-    .update({ attendance_num: supabaseClient.increment(-1) })
-    .eq('id', id)
-    .select();
+async function decreaseAttendance() {
+  try {
+    const response = await fetch("https://script.google.com/macros/s/AKfycbztzl7tuBZDQDlSsfsHbfzwyZoP4Owb1bC-Q7zH-Knu8n8Q6i7xhf-yff8WhfnAOv4MoA/exec", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ change: -1 })
+    });
 
-  if (error) {
-    console.log("Error:", error);
-  } else {
-    console.log("Updated:", data);
+    const result = await response.json();
+
+    if (result.success) {
+      alert("Attendance sucesfully recorded!");
+    } else {
+      alert("Sorry, a problem has occured. Please check back in a few minutes.");
+    }
+
+  } catch (error) {
+    console.error("Decrease error:", error);
+    alert("Network or script error");
   }
 }
