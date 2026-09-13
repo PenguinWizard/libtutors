8c755b007b1c16a7751b6604f0b22d02144f0ee4
ArmanPefourque <armanpefo@gmail.com>
2026-09-13 09:36:03 -0700

    Implemented study room filter

--- script.js
+++ script.js
@@ -21,9 +21,27 @@ window.addEventListener("scroll", () => {
     }
 });
 
-document.getElementById("closebutton1").addEventListener("click", () => {
-    window.location.href = "https://cambriantutors.org/book.html"
-})
-document.getElementById("closebutton2").addEventListener("click", () => {
-    window.location.href = "https://cambriantutors.org/book.html"
-})
+const closeBtn1 = document.getElementById("closebutton1");
+    if (closeBtn1) {
+        document.getElementById("closebutton1").addEventListener("click", () => {
+            window.location.href = "https://cambriantutors.org/book.html"
+        })
+}
+
+const closeBtn2 = document.getElementById("closebutton2");
+    if (closeBtn2) {
+        document.getElementById("closebutton2").addEventListener("click", () => {
+            window.location.href = "https://cambriantutors.org/book.html"
+        })
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("room-select").addEventListener("change", function() {
+        console.log("Selected:", this.value);
+        if (this.value == 1) {
+            document.getElementById("book-iframe").src = "https://calendar.google.com/calendar/appointments/schedules/AcZssZ0O968oYxWudUFfR8VQwj8JkXswU-6aP6G9OYIquIectBmVetj2bVsFogsDUx2ncyMsxcREz0-f?gv=true";
+        } else {
+            document.getElementById("book-iframe").src = "https://calendar.google.com/calendar/appointments/schedules/AcZssZ1F-feqIpVPjy7GzK7Yeuz4ZTZcF5bC8PGWoSgFltXBeqRK7_NUl0Bl-SXSWpYtoGQKb5upUsqV?gv=true";
+        }
+    });
+});
